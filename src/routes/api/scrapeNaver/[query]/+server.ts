5d8f5df2d1022dd5cc9eb8cb7e213e3895540078
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-07-10 17:16:10 +0900

    fix(scraping): use get instead of post on the api
    In production, keep getting a 500 error that get is not allowed, so
    I have removed the call to the post api.

---
+++ src/routes/api/scrapeNaver/[query]/+server.ts
@@ -0,0 +1,11 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from '@sveltejs/kit';
+import { scrapeNaver } from '$lib';
+
+export const GET: RequestHandler = async({ request, params }) => {
+	const queryTerm = params.query;
+	const links = await scrapeNaver(queryTerm);
+
+	return json(links);
+}
+
--- src/routes/scrape/+page.ts
+++ src/routes/scrape/+page.ts
@@ -3,15 +3,18 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async ({ fetch, url }) => {
 	const searchParams = url.searchParams.get('searchNaverFor');
 
+	/*
 	const response = await fetch('/api/scrapeNaver', {
 		method: 'POST',
 		body: JSON.stringify(searchParams),
 	});
+ */
 
+	const getResponse = await fetch(`/api/scrapeNaver/${searchParams}`)
 
 	return {
 		title: `Links for ${searchParams}`,
-		links: await response.json(),
+		links: await getResponse.json(),
 		searchedFor: searchParams,
 	}
 
